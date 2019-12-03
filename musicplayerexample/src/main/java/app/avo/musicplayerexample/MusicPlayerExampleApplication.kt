053ed883ceb8fabae97c173f1115043cf1fe3068
Alexey Verein <tpom6oh@gmail.com>
2019-12-03 16:40:31 +0300

    Add kotlin avo file and update java

--- musicplayerexample/src/main/java/app/avo/musicplayerexample/MusicPlayerExampleApplication.kt
+++ musicplayerexample/src/main/java/app/avo/musicplayerexample/MusicPlayerExampleApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 
 import app.avo.androidanalyticsdebugger.DebuggerManager
 import sh.avo.Avo
+import sh.avo.AvoEnv
+import sh.avo.ICustomDestination
 
 class MusicPlayerExampleApplication : Application() {
 
@@ -12,22 +14,23 @@ class MusicPlayerExampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Avo.initAvo(Avo.AvoEnv.DEV, object: Avo.ICustomDestination {
-            override fun make(env: Avo.AvoEnv?) {
-            }
+          Avo.initAvo(AvoEnv.DEV,
+                object: ICustomDestination {
+                    override fun make(env: AvoEnv) {
+                    }
 
-            override fun logEvent(eventName: String?, eventProperties: MutableMap<String, Any>?) {
-            }
+                    override fun logEvent(eventName: String, eventProperties: Map<String, *>) {
+                    }
 
-            override fun setUserProperties(userId: String?, userProperties: MutableMap<String, Any>?) {
-            }
+                    override fun setUserProperties(userId: String, userProperties: Map<String, *>) {
+                    }
 
-            override fun identify(userId: String?) {
-            }
+                    override fun identify(userId: String) {
+                    }
 
-            override fun unidentify() {
-            }
-        }, debugger)
+                    override fun unidentify() {
+                    }
+            }, debugger)
         Avo.appOpened()
     }
 }
